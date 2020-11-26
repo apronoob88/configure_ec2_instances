3ec9be890f5f77431bb22b204549cc38c9de611f
joeljhanster <50553469+joeljhanster@users.noreply.github.com>
2020-11-26 18:28:47 +0800

    add MacOS setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,23 @@
-sudo apt-get update
-sudo apt-get install -y python3-pip
-python3 -m pip install boto3
-sudo apt-get install -y python3-paramiko
+#!/bin/bash
+
+# Linux Setup
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+then
+  sudo apt-get update
+  sudo apt-get install -y python3-pip
+  python3 -m pip install boto3
+  sudo apt-get install -y python3-paramiko
+
+# MacOS Setup
+elif [[ "$OSTYPE" == "darwin"* ]]
+then
+  brew install python3
+  python3 -m pip install boto3
+  python3 -m pip install paramiko
+
+else
+  echo "This script is only for Unix based systems"
+fi
 
 wget https://raw.githubusercontent.com/apronoob88/configure_ec2_instances/master/configure_ec2_instances.py
 python3 configure_ec2_instances.py
