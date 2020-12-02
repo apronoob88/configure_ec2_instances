a091642f633834916dc3d34233ad346bf81a0ed1
joeljhanster <50553469+joeljhanster@users.noreply.github.com>
2020-12-03 04:58:46 +0800

    update create tables

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y mysql-server
 
 echo "Initializing root user with privileges"
 sudo mysql -e 'update mysql.user set plugin = "mysql_native_password" where user = "root"'
-sudo mysql -e 'create user "root"@"%" identified by ""'
+sudo mysql -e 'create user if not exists "root"@"%" identified by ""'
 sudo mysql -e 'grant all privileges on *.* to "root"@"%" with grant option'
 sudo mysql -e 'flush privileges'
 sudo service mysql restart
@@ -18,29 +18,29 @@ echo "Access MySQL using root"
 mysql -u root << EOF
 CREATE DATABASE IF NOT EXISTS bookwormies;
 USE bookwormies;
-CREATE TABLE IF NOT EXISTS `reviewers` (
-  `reviewer_id` VARCHAR(45) NOT NULL,
-  `reviewer_name` VARCHAR(45) NOT NULL,
-  `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`reviewer_id`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
+CREATE TABLE IF NOT EXISTS reviewers (
+  reviewer_id VARCHAR(45) NOT NULL,
+  reviewer_name VARCHAR(45) NOT NULL,
+  username VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  PRIMARY KEY (reviewer_id),
+  UNIQUE KEY username_UNIQUE (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `reviews` (
-  `review_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `asin` VARCHAR(45) NOT NULL,
-  `likes` INT NOT NULL DEFAULT '0',
-  `dislikes` INT NOT NULL DEFAULT '0',
-  `overall_rating` INT NOT NULL DEFAULT '0',
-  `review_text` VARCHAR(8000) NOT NULL,
-  `review_time` DATE NOT NULL,
-  `reviewer_id` VARCHAR(45) NOT NULL,
-  `summary` VARCHAR(8000) DEFAULT NULL,
-  `unix_review_time` INT DEFAULT NULL,
-  PRIMARY KEY (`review_id`),
-  FOREIGN KEY (`reviewer_id`) REFERENCES `reviewers`(`reviewer_id`),
-  KEY `idx_asin` (`asin`)
+CREATE TABLE IF NOT EXISTS reviews (
+  review_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  asin VARCHAR(45) NOT NULL,
+  likes INT NOT NULL DEFAULT '0',
+  dislikes INT NOT NULL DEFAULT '0',
+  overall_rating INT NOT NULL DEFAULT '0',
+  review_text VARCHAR(8000) NOT NULL,
+  review_time DATE NOT NULL,
+  reviewer_id VARCHAR(45) NOT NULL,
+  summary VARCHAR(8000) DEFAULT NULL,
+  unix_review_time INT DEFAULT NULL,
+  PRIMARY KEY (review_id),
+  FOREIGN KEY (reviewer_id) REFERENCES reviewers(reviewer_id),
+  KEY idx_asin (asin)
 ) ENGINE=InnoDB AUTO_INCREMENT=982632 DEFAULT CHARSET=utf8;
 
 EOF
